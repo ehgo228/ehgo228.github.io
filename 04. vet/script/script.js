fec9adb886d93b3711aab1917b71221eff73d076
ehgo228 <167166145+ehgo228@users.noreply.github.com>
2024-04-16 15:07:19 +0900

    Add files via upload

---
+++ 04. vet/script/script.js
@@ -0,0 +1,46 @@
+//자바
+
+//메뉴
+$(".navi li").mouseover(function(){
+    $(".sub, .bg").stop().slideDown();
+})
+$(".navi li").mouseout(function(){
+    $(".sub, .bg").stop().slideUp();
+})
+$(".navi li").focusin(function(){
+    $(".sub, .bg").stop().slideDown();
+})
+$(".navi li").focusout(function(){
+    $(".sub, .bg").stop().slideUp();
+})
+
+//슬라이드
+$("#slide li").hide();
+$("#slide li").eq(0).show();
+var imgs = 0;
+function slide(){
+    if(imgs<2){imgs++;}else{imgs=0;}
+    $("#slide li").fadeOut();
+    $("#slide li").eq(imgs).fadeIn();
+}
+setInterval(slide,2000)
+
+//탭메뉴
+var tab = 0;
+$(".tab li").click(function(){
+    $(".tab li").removeClass("on")
+    $(this).addClass("on");
+
+    tab=$(this).index();
+    $(".tabcon").hide();
+    $(".tabcon").eq(tab).show();
+    return false();
+})
+
+//팝업
+$(".cc").click(function(){
+    $(".modal").show();
+})
+$(".bt button").click(function(){
+    $(".modal").hide();
+})
